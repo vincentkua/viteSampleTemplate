352f3c094d7685c3ead323474fed8a80e673e3ef
vincentkua <zylyn89@gmail.com>
2024-01-13 19:38:43 +0800

    init: initiate with vite and simplied

---
+++ src/bootstrap.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Providers from './Providers'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Providers />
+  </React.StrictMode>,
+)
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <>
+      <h3>Home Page</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+        sapiente dolor nemo deleniti odit. Voluptatum dolores est alias
+        assumenda possimus. Id praesentium voluptatum omnis debitis? Mollitia,
+        ipsum labore! Quaerat, ipsam.
+      </p>
+    </>
+  );
+};
+
+export default Home;
